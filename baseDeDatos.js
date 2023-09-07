a392fea7f65965d3a29e4c5d9fa633b34891d809
Ezequiel Mascotena <ezequielmascotena@gmail.com>
2023-09-07 01:28:31 -0300

    se agregaron algunos elementos del html, se creo un archivo de base de datos para productos y se optimizo el cod de JS segun lo requerido

---
+++ baseDeDatos.js
@@ -0,0 +1,43 @@
+const productos = [{
+    id: 1,
+    nombre: "Victoria's Secret (Algodón)",
+    foto: "./img/vs.jpeg",
+    descripcion: "Docena de less VS de algodón, con elastico afelpado de 30mm. La docena viene surtida en 3 colores, negro, blanco y gris (4 unidades de cada color).",
+    precio: 5600,
+},
+{
+    id: 2,
+    nombre: "Calvin Klein (Algodón)",
+    foto: "./img/ck.jpg",
+    descripcion: "Docena de less CK de algodón, con elastico afelpado de 30mm. La docena viene surtida en 3 colores, negro, blanco y gris (4 unidades de cada color).",
+    precio: 5600,
+},
+{
+    id: 3,
+    nombre: "Pink (Algodón)",
+    foto: "./img/pink.jpeg",
+    descripcion: "Docena de less Pink de algodón, con elastico afelpado de 30mm. La docena viene surtida en 3 colores, negro, blanco y gris (4 unidades de cada color).",
+    precio: 5600,
+},
+{
+    id: 4,
+    nombre: "Tommy (Algodón)",
+    foto: "./img/tommy.jpeg",
+    descripcion: "Docena de less Tommy de algodón, con elastico afelpado de 30mm. La docena viene surtida en 3 colores, negro, blanco y gris (4 unidades de cada color).",
+    precio: 5600,
+},
+{
+    id: 5,
+    nombre: "Pink (Puntilla)",
+    foto: "./img/pinkpuntilla.jpeg",
+    descripcion: "Docena de less Pink de puntilla, con elastico afelpado de 30mm. La docena viene surtida en 3 colores, negro, blanco y rojo (4 unidades de cada color).",
+    precio: 6000,
+},
+{
+    id: 6,
+    nombre: "Victoria's Secret (Puntilla)",
+    foto: "./img/vspunt.jpeg",
+    descripcion: "Docena de less Victoria's Secret de puntilla, con elastico afelpado de 30mm. La docena viene surtida en 3 colores, negro, blanco y rojo (4 unidades de cada color).",
+    precio: 6000,
+},
+]; 
--- codigo.js
+++ codigo.js
@@ -1,30 +1,60 @@
+/* codigo afterclass
+
+function filtrarPrecioMax(precioMaximo){
+    const filtrados = productos.filter ((producto) => producto.precio <=precioMaximo);
+    console.table(filtrados);
+}
+
+let precio = parseFloat(prompt("ingrese un rango maximo del precio que desea (0-salir)"))
+while (precio!=0){
+    filtrarPrecioMax(precio);
+    precio = parseFloat(prompt("ingrese un rango maximo del precio que desea (0-salir)"))
+}*/
+
+let carrito = [];
+
+
 
 let total = 0;
-const multiplicacion = (producto, cantidad) => producto * cantidad;
+const multiplicar = (precio, cant) => precio * cant;
 
-let producto = prompt("Seleccione el producto que desea comprar:\n1- Conjunto Victoria's Secret. ($7.000.-)\n2- Pack x6 Less Calvin Klein. ($5.000.-) \n3- Pack x3 Less Pink. ($3.000.-) \n4- Bikini Animal Print ($6.000.-) \n0- ir a Pagar.");
-while (producto != "0") {
-    switch (producto) {
+let opciones ="";
+productos.forEach(producto => {
+    opciones += `${producto.id}- ${producto.nombre} $${producto.precio}.-\n`
+});
+
+
+let compra = prompt(`${opciones}\n 0- ir a Pagar.`);
+while (compra != "0") {
+    switch (compra) {
         case "1":
             cant = prompt("ingrese la cantidad de unidades que desea");
-            total += multiplicacion(7000, cant);
+            total += multiplicar(5600, cant);
             break;
         case "2":
             cant = prompt("ingrese la cantidad de unidades que desea");
-            total += multiplicacion(5000, cant);
+            total += multiplicar(5600, cant);
             break;
         case "3":
             cant = prompt("ingrese la cantidad de unidades que desea");
-            total += multiplicacion(3000, cant);
+            total += multiplicar(5600, cant);
             break;
         case "4":
             cant = prompt("ingrese la cantidad de unidades que desea");
-            total += multiplicacion(6000, cant);
+            total += multiplicar(5600, cant);
+            break;
+        case "5":
+            cant = prompt("ingrese la cantidad de unidades que desea");
+            total += multiplicar(6000, cant);
+            break;
+        case "6":
+            cant = prompt("ingrese la cantidad de unidades que desea");
+            total += multiplicar(6000, cant);
             break;
         default:
             alert("el num que ingreso no es valido");
     }
-    producto = prompt("Seleccione el producto que desea comprar:\n1- Conjunto Victoria's Secret. ($7.000.-)\n2- Pack x6 Less Calvin Klein. ($5.000.-) \n3- Pack x3 Less Pink. ($3.000.-) \n4- Bikini Animal Print ($6.000.-) \n0- ir a Pagar.");
+    compra = prompt(`${opciones}\n 0- ir a Pagar.`);
 }
 
-alert ("El total de su compra es $"+ total)
+alert("El total de su compra es $" + total)
